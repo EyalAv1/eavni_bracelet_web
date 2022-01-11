1b4c219a19d4a8e6e1081a050e09e18d64d531a7
EyalAv1 <eyalavni6@gmail.com>
2022-01-12 00:51:12 +0200

    init

--- src/components/Products/ProductsOrderPage/OrderPage.js
+++ src/components/Products/ProductsOrderPage/OrderPage.js
@@ -1,15 +1,25 @@
-import React from 'react'; 
-
+import React from 'react';
 // import Aux from '../../hoc/Auxuliary/Auxuliary';
+import useFirestore from '../../../hooks/useFirebase'
+import Image from '../../UI/Image/Image';
 
-const homePage = props => {
+const HomePage = props => {
+
+    const { docs } = useFirestore('images');
+    let search = window.location.search;
+    let params = new URLSearchParams(search);
+    let foo = params.get('ImgName');;
 
     return (
         <label>
             <h1 style={{size: '50px', color: 'black'}}>Welcome to EAvni</h1>
             <h2>this is product order page</h2>
+            {docs && docs.map((doc) => (
+                (foo === doc.imgName) ? <Image url={doc.url} /> : null
+            ))}
+            <h3>here will be the description about the bracelet</h3>
         </label>
     );
 };
 
-export default homePage;
+export default HomePage;
