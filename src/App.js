b66f6b6189b51524610416c1afa697eee00218f6
EyalAv1 <eyalavni6@gmail.com>
2022-01-13 23:21:05 +0200

    init

--- src/App.js
+++ src/App.js
@@ -37,9 +37,9 @@ const App = (props) => {
     <Switch>
       <Route path="/auth" render={(props) => <Auth {...props}/>} />
       {/* <Route path="/shop" render={(props) => <Shop {...props}/>} /> */}
-      <Route path="/shop" component={Shop} />
+      <Route path="/shop" exact component={Shop} />
       {/* <Route path="/orderPage" render={(props) => <OrderPage {...props}/>} /> */}
-      <Route path="/orderPage" component={OrderPage} />
+      <Route path="/orderPage" exact component={OrderPage} />
       <Route path="/" exact component={HomePage} />
       <Redirect to="/" />
     </Switch>
